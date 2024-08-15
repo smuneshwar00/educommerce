c0734f9a2849bbb7a0c6139022742e64a8cf1129
Swapnil Muneshwar <smuneshwar00@gmail.com>
2024-08-16 02:14:58 +0530

    created components for header, menu and user profile

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edu-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
---
+++ src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edu-user-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+}

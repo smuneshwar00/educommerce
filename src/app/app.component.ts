8331a622fdcebfa4c444be9b2f8fade6c9db8438
Swapnil Muneshwar <smuneshwar00@gmail.com>
2024-08-16 20:46:31 +0530

    Added hero banner with Text content.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TestChildComponent } from "./components/test-child/test-child.component";
+import { HeroBannerComponent } from "./components/hero-banner/hero-banner.component";
 
 @Component({
   selector: 'edu-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TestChildComponent],
+  imports: [RouterOutlet, HeaderComponent, TestChildComponent, HeroBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/hero-banner/hero-banner.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'edu-hero-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-banner.component.html',
+  styleUrl: './hero-banner.component.css'
+})
+export class HeroBannerComponent {
+  @Input() heading : string = "";
+  @Input() caption !: string;
+  @Input() buttonText : string | null = null;
+}

185c36ba4cf0624cec490ac1877ac4130241a590
Swapnil Muneshwar <smuneshwar00@gmail.com>
2024-08-24 21:38:06 +0530

    Added Product Details in data json and fetched with http

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+import { CommonModule } from '@angular/common';
+
+export interface IProduct{
+  id: number,
+  name: string,
+}
 
 @Component({
   selector: 'edu-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  activateRoute = inject(ActivatedRoute);
+  _productService = inject(ProductsService);
 
+  product: null | IProduct = null;
+
+  ngOnInit(): void {
+
+    const { id } = this.activateRoute.snapshot.params;
+    this._productService.getProductById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ConfigService } from './config.service';
+import { IProduct } from '../components/product-detail/product-detail.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  _http = inject(HttpClient);
+  _config = inject(ConfigService);
+
+  getProductById(id: string) {
+    return this._http.get<IProduct>(`${this._config.API_PRODUCT_URL}/${id}`);
+  }
+}

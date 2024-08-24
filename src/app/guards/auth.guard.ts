14decdf380aa75c178a7bcd80f78c18072a9f5ff
Swapnil Muneshwar <smuneshwar00@gmail.com>
2024-08-24 22:27:31 +0530

    Added router navigateByUrl to load home page correctly with details

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true; // this will allow page access
+  const router = inject(Router);
+
+  const authState = false;
+
+  if(authState){
+    //redirects to page
+    return true;
+  }else{
+    router.navigateByUrl("/");
+    return false;
+  }
+  //return true; // this will allow page access
   //return false; // this will redirect you to home page
 };
